d41b5efe5d7cc08cca70c3d527e50251c2402e73
codewthR <rahulbhatia.ramm@gmail.com>
2024-02-25 12:07:35 +0530

    Qr code

---
+++ QRCODE.py
@@ -0,0 +1,23 @@
+import qrcode
+
+# Data to encode in the QR code
+data = "https://github.com/codewthR"
+
+# Generate QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image
+img.save("example_qr_code.png")
+
+print("QR Code generated successfully.")
+
