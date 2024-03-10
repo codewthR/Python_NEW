ea7b6001d324b391210c51bc73ffdb5b2b2103ae
Rahul <121219750+codewthR@users.noreply.github.com>
2024-03-11 02:29:33 +0530

    speech recognition.py

--- speech recognition.py
+++ speech recognition.py
@@ -1,14 +1,9 @@
 import speech_recognition as sr
-
-# initialize the recognizer
 r = sr.Recognizer()
-
 # open the default microphone as the audio source
 with sr.Microphone() as source:
     print("Speak something:")
-    # record the audio
     audio_data = r.record(source, duration=5)
     print("Recognizing...")
-    # convert speech to text
     text = r.recognize_google(audio_data)
     print(text)
